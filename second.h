070b5cc21553b4c99e49fd73d3707e3aaa1e9ad6
trox <masalox5@gmail.com>
2025-01-30 18:43:53 +0300

    Create second.h

---
+++ second.h
@@ -0,0 +1,21 @@
+    #pragma once
+
+    class zayac {
+    public:
+        int* zayaccount;
+        int hardcore;
+        zayac();               // Конструктор
+        void addzayac();
+    };
+    void menu();
+    int game1(int& corm, int& zayaccount, int& money, int gold1, int gold2, int cofecent);
+    int game2();
+    void magazine();
+    int notmoney(int gold, int price);
+    void ClearScreen();
+    void runRabbitSystem(int& zayaccount, int gold1, int hardcore);
+    void clearScreen();
+    void last();
+    void second();
+    void secondtwo();
+    void first();
